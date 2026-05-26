acb2dc008c84b59532f4ac5b76740ad9b4a34aca
Sushil Sharma <sushilsharma@Sushils-MacBook-Air.local>
2026-05-26 15:50:51 +0545

    fixing admin seeding

--- Backend/src/Database/database.ts
+++ Backend/src/Database/database.ts
@@ -1,24 +1,51 @@
 import mongoose from "mongoose";
 import envConfig from "../Config/Config";
-
-
-
-
+import User from "../Controller/User/UserModel";
+import bcrypt from "bcryptjs";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(envConfig.mongodbUrl as string, {
-            connectTimeoutMS: 5000
-        });
-       
+  try {
 
-        console.log("MongoDB connected successfully");
+    await mongoose.connect(envConfig.mongodbUrl as string, {
+      connectTimeoutMS: 5000,
+    });
 
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-        process.exit(1);
+    console.log("MongoDB connected successfully");
+
+    // check admin
+    const isAdminExist = await User.findOne({
+      Email: "admin@gmail.com",
+    });
+
+    if (!isAdminExist) {
+
+      // hash password
+      const salt = await bcrypt.genSalt(10);
+
+      const hashedPassword = await bcrypt.hash(
+        "admin123",
+        salt
+      );
+
+      // create admin
+      await User.create({
+        Email: "admin@gmail.com",
+        UserPhoneNumber: "1234567890",
+        password: hashedPassword,
+        UserName: "Admin",
+        role: "admin",
+      });
+
+      console.log("Admin seeded successfully");
+
+    } else {
+      console.log("Admin already exists");
     }
+
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
 };
 
-export default connectDB;   
-
+export default connectDB;
