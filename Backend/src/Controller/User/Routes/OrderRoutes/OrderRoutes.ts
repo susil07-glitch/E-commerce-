8cda1e8aaf5ee922d0c74f0c0b907114c124542a
Sushil Sharma <214009577+susil07-glitch@users.noreply.github.com>
2026-06-07 13:52:24 +0545

    Fixing bug

--- Backend/src/Controller/User/Routes/OrderRoutes/OrderRoutes.ts
+++ Backend/src/Controller/User/Routes/OrderRoutes/OrderRoutes.ts
@@ -6,10 +6,10 @@ import isAuthenticated from '../../../../MiddleWare/isAuthenticated';
 
 const orderRoutes=express.Router();
 
-orderRoutes.route("/order/create").post(isAuthenticated,createOrder)
-orderRoutes.route("/order/myorders").get(isAuthenticated,getMyOrders)
-orderRoutes.route("/order/updateOrder").patch(isAuthenticated,updateMyOrder)
-orderRoutes.route("/order/cancelOrder").patch(isAuthenticated,cancelMyOrder)
+orderRoutes.route("/create/:id").post(isAuthenticated,createOrder)
+orderRoutes.route("/myorders").get(isAuthenticated,getMyOrders)
+orderRoutes.route("/updateOrder").patch(isAuthenticated,updateMyOrder)
+orderRoutes.route("/cancelOrder").patch(isAuthenticated,cancelMyOrder)
 
 
 
